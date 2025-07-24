81059d54a0194a7b5389aa398703d86daa51d19c
hmvvong <hanmowang1998@gmail.com>
2025-07-24 15:13:33 -0600

    .

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -8,8 +8,8 @@ export const metadata = {
   title: 'Edit',
 }
  
-export default async function Page(p: { params: { id: string } }) {
-    const params = p.params;
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+    const params =  await props.params;
     const id = params.id;
     const [invoice, customers] = await Promise.all([
         fetchInvoiceById(id),
